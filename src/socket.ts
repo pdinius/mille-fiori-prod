fea2873da565587106f44bdf9ec1c8673b6bfc70
dinx <phildinius@gmail.com>
2023-12-09 16:29:48 -0500

    Update socket.ts

--- src/socket.ts
+++ src/socket.ts
@@ -2,7 +2,7 @@ import { io, Socket } from "socket.io-client";
 import { ClientToServerEvents, ServerToClientEvents } from "./types";
 
 const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
-  `http://localhost:3000`
+  `https://server-production-0918.up.railway.app/`
 );
 localStorage.clear();
 const id = localStorage.getItem("game-id");
